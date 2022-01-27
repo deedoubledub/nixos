b13191b1097c2fabe8ddf2e4454083f0112cde6f
David Wagner <dwagner86@gmail.com>
2022-01-27 14:45:26 -0500

    renames dock script to tbdock.sh

--- dock.nix
+++ dock.nix
@@ -5,8 +5,8 @@
 {
   services.udev = {
     extraRules = ''
-      ACTION=="add", SUBSYSTEM=="thunderbolt", ATTRS{vendor}=="0x177", ATTRS{device}=="0x428a", RUN+="${pkgs.bash}/bin/bash /home/dwagner/.local/bin/dock.sh"
-      ACTION=="remove", SUBSYSTEM=="thunderbolt", ENV{DEVPATH}=="/devices/pci0000:00/0000:00:0d.3/domain1/1-0/1-3", RUN+="${pkgs.bash}/bin/bash /home/dwagner/.local/bin/dock.sh"
+      ACTION=="add", SUBSYSTEM=="thunderbolt", ATTRS{vendor}=="0x177", ATTRS{device}=="0x428a", RUN+="${pkgs.bash}/bin/bash /home/dwagner/.local/bin/tbdock.sh"
+      ACTION=="remove", SUBSYSTEM=="thunderbolt", ENV{DEVPATH}=="/devices/pci0000:00/0000:00:0d.3/domain1/1-0/1-3", RUN+="${pkgs.bash}/bin/bash /home/dwagner/.local/bin/tbdock.sh"
     '';
   };
 }
