7ba3a6fc62af9ed51f89e0a88f873bd9d66bb3f9
David Wagner <dwagner86@gmail.com>
2023-02-03 21:58:47 -0500

    adds firewall config for wireguard

--- network.nix
+++ network.nix
@@ -11,5 +11,20 @@
   networking.interfaces.wlp170s0.useDHCP = true;
 
   networking.networkmanager.enable = true;
+
+  # wireguard firewall configuration
+  networking.firewall = {
+    # if packets are still dropped, they will show up in dmesg
+    logReversePathDrops = true;
+    # wireguard trips rpfilter up
+    extraCommands = ''
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+    '';
+    extraStopCommands = ''
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+    '';
+  };
 }
 
